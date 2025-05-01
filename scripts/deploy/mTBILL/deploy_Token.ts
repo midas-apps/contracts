3541ecbcce71b2c63ef9f40f7e55be270f342146
kostyamospan <kostya04042001@gmail.com>
2025-05-01 17:19:39 +0300

    refactor: mtoken deploy scripts

--- scripts/deploy/mTBILL/deploy_Token.ts
+++ scripts/deploy/mTBILL/deploy_Token.ts
@@ -5,7 +5,7 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { deployMToken } from '../common/token';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployMToken(hre, 'mMEV');
+  await deployMToken(hre, 'mTBILL');
 };
 
 func(hre).then(console.log).catch(console.error);
