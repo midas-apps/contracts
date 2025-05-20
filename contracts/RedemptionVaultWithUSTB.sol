4c547dd1ec148c54e6eff72dbbdeb20b85c51d99
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-05-20 13:24:57 +0300

    feat: ustb redemption

---
+++ contracts/RedemptionVaultWithUSTB.sol
@@ -0,0 +1,203 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import {IERC20Upgradeable as IERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {SafeERC20Upgradeable as SafeERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+import "./RedemptionVault.sol";
+
+import "./interfaces/ustb/IRedemption.sol";
+import "./libraries/DecimalsCorrectionLibrary.sol";
+
+/**
+ * @title RedemptionVaultWithUSTB
+ * @notice Smart contract that handles mTBILL redemptions using USTB
+ * @author RedDuck Software
+ */
+contract RedemptionVaultWithUSTB is RedemptionVault {
+    using DecimalsCorrectionLibrary for uint256;
+    using SafeERC20 for IERC20;
+
+    /**
+     * @notice USTB redemption contract interface
+     */
+    IRedemption public ustbRedemption;
+
+    /**
+     * @notice USDC token address that this contract uses for redemptions
+     */
+    address public usdcToken;
+
+    /**
+     * @notice USTB token address
+     */
+    address public ustbToken;
+
+    uint256[48] private __gap;
+
+    /**
+     * @notice upgradeable pattern contract`s initializer
+     * @param _ac address of MidasAccessControll contract
+     * @param _mTokenInitParams init params for mToken
+     * @param _receiversInitParams init params for receivers
+     * @param _instantInitParams init params for instant operations
+     * @param _sanctionsList address of sanctionsList contract
+     * @param _variationTolerance percent of prices diviation 1% = 100
+     * @param _minAmount basic min amount for operations
+     * @param _fiatRedemptionInitParams params fiatAdditionalFee, fiatFlatFee, minFiatRedeemAmount
+     * @param _ustbRedemption USTB redemption contract address
+     * @param _requestRedeemer address is designated for standard redemptions, allowing tokens to be pulled from this address
+     * @param _usdcToken USDC token address
+     * @param _ustbToken USTB token address
+     */
+    function initialize(
+        address _ac,
+        MTokenInitParams calldata _mTokenInitParams,
+        ReceiversInitParams calldata _receiversInitParams,
+        InstantInitParams calldata _instantInitParams,
+        address _sanctionsList,
+        uint256 _variationTolerance,
+        uint256 _minAmount,
+        FiatRedeptionInitParams calldata _fiatRedemptionInitParams,
+        address _requestRedeemer,
+        address _ustbRedemption,
+        address _usdcToken,
+        address _ustbToken
+    ) external initializer {
+        __RedemptionVault_init(
+            _ac,
+            _mTokenInitParams,
+            _receiversInitParams,
+            _instantInitParams,
+            _sanctionsList,
+            _variationTolerance,
+            _minAmount,
+            _fiatRedemptionInitParams,
+            _requestRedeemer
+        );
+        _validateAddress(_ustbRedemption, false);
+        _validateAddress(_usdcToken, false);
+        _validateAddress(_ustbToken, false);
+        ustbRedemption = IRedemption(_ustbRedemption);
+        usdcToken = _usdcToken;
+        ustbToken = _ustbToken;
+    }
+
+    /**
+     * @notice redeem mToken to USDC if daily limit and allowance not exceeded
+     * If contract doesn't have enough USDC, USTB redemption flow will be triggered
+     * Burns mToken from the user.
+     * Transfers fee in mToken to feeReceiver
+     * Transfers tokenOut to user.
+     * @param tokenOut token out address
+     * @param amountMTokenIn amount of mToken to redeem
+     * @param minReceiveAmount minimum expected amount of tokenOut to receive (decimals 18)
+     */
+    function redeemInstant(
+        address tokenOut,
+        uint256 amountMTokenIn,
+        uint256 minReceiveAmount
+    )
+        external
+        override
+        whenFnNotPaused(this.redeemInstant.selector)
+        onlyGreenlisted(msg.sender)
+        onlyNotBlacklisted(msg.sender)
+        onlyNotSanctioned(msg.sender)
+    {
+        address user = msg.sender;
+
+        require(tokenOut == usdcToken, "RVU: invalid token");
+
+        (
+            uint256 feeAmount,
+            uint256 amountMTokenWithoutFee
+        ) = _calcAndValidateRedeem(user, tokenOut, amountMTokenIn, true, false);
+
+        _requireAndUpdateLimit(amountMTokenIn);
+
+        uint256 tokenDecimals = _tokenDecimals(tokenOut);
+
+        uint256 amountMTokenInCopy = amountMTokenIn;
+        address tokenOutCopy = tokenOut;
+        uint256 minReceiveAmountCopy = minReceiveAmount;
+
+        (uint256 amountMTokenInUsd, uint256 mTokenRate) = _convertMTokenToUsd(
+            amountMTokenInCopy
+        );
+        (uint256 amountTokenOut, uint256 tokenOutRate) = _convertUsdToToken(
+            amountMTokenInUsd,
+            tokenOutCopy
+        );
+
+        _requireAndUpdateAllowance(tokenOutCopy, amountTokenOut);
+
+        mToken.burn(user, amountMTokenWithoutFee);
+        if (feeAmount > 0)
+            _tokenTransferFromUser(address(mToken), feeReceiver, feeAmount, 18);
+
+        uint256 amountTokenOutWithoutFee = (amountMTokenWithoutFee *
+            mTokenRate) / tokenOutRate;
+
+        require(
+            amountTokenOutWithoutFee >= minReceiveAmountCopy,
+            "RVU: minReceiveAmount > actual"
+        );
+
+        uint256 amountTokenOutWithoutFeeFrom18 = amountTokenOutWithoutFee
+            .convertFromBase18(tokenDecimals);
+
+        _checkAndRedeemUSTB(tokenOutCopy, amountTokenOutWithoutFeeFrom18);
+
+        _tokenTransferToUser(
+            tokenOutCopy,
+            user,
+            amountTokenOutWithoutFeeFrom18.convertToBase18(tokenDecimals),
+            tokenDecimals
+        );
+
+        emit RedeemInstant(
+            user,
+            tokenOutCopy,
+            amountMTokenInCopy,
+            feeAmount,
+            amountTokenOutWithoutFee
+        );
+    }
+
+    /**
+     * @notice Check if contract has enough USDC balance for redeem
+     * if not, trigger USTB redemption flow to redeem exactly the missing amount
+     * @param tokenOut tokenOut address
+     * @param amountTokenOut amount of tokenOut needed
+     */
+    function _checkAndRedeemUSTB(
+        address tokenOut,
+        uint256 amountTokenOut
+    ) internal {
+        uint256 contractBalanceTokenOut = IERC20(tokenOut).balanceOf(
+            address(this)
+        );
+        if (contractBalanceTokenOut >= amountTokenOut) return;
+
+        uint256 missingAmount = amountTokenOut - contractBalanceTokenOut;
+        (uint256 ustbToRedeem, ) = ustbRedemption.calculateUstbIn(
+            missingAmount
+        );
+
+        IERC20 ustb = IERC20(ustbToken);
+        uint256 ustbBalance = ustb.balanceOf(address(this));
+
+        if (ustbBalance < ustbToRedeem) {
+            revert("RVU: insufficient USTB balance");
+        }
+
+        (uint256 maxUstbAmount, ) = ustbRedemption.maxUstbRedemptionAmount();
+        if (ustbToRedeem > maxUstbAmount) {
+            revert("RVU: insufficient liquidity in redemption contract");
+        }
+
+        ustb.safeIncreaseAllowance(address(ustbRedemption), ustbToRedeem);
+        ustbRedemption.redeem(ustbToRedeem);
+    }
+}
