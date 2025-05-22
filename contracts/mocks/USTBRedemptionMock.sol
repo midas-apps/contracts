0e5fc3577bf9f96ce4a0cb6d9600dccdcb1a443a
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-05-22 14:34:55 +0300

    fix: mock import

--- contracts/mocks/USTBRedemptionMock.sol
+++ contracts/mocks/USTBRedemptionMock.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "../interfaces/ustb/IRedemption.sol";
+import "../interfaces/ustb/IUSTBRedemption.sol";
 
 contract USTBRedemptionMock {
     using SafeERC20 for IERC20;
