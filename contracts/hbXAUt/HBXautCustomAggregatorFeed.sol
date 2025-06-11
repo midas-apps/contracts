4e46cbe9f9af73c04b13ade2bfa5074f99750546
kostyamospan <kostyamospan@users.noreply.github.com>
2025-06-11 11:14:55 +0300

    feat: xbxaut contracts

---
+++ contracts/hbXAUt/HBXautCustomAggregatorFeed.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "../feeds/CustomAggregatorV3CompatibleFeed.sol";
+import "./HBXautMidasAccessControlRoles.sol";
+
+/**
+ * @title HBXautCustomAggregatorFeed
+ * @notice AggregatorV3 compatible feed for hbXAUt,
+ * where price is submitted manually by feed admins
+ * @author RedDuck Software
+ */
+contract HBXautCustomAggregatorFeed is
+    CustomAggregatorV3CompatibleFeed,
+    HBXautMidasAccessControlRoles
+{
+    /**
+     * @dev leaving a storage gap for futures updates
+     */
+    uint256[50] private __gap;
+
+    /**
+     * @inheritdoc CustomAggregatorV3CompatibleFeed
+     */
+    function feedAdminRole() public pure override returns (bytes32) {
+        return HB_XAUT_CUSTOM_AGGREGATOR_FEED_ADMIN_ROLE;
+    }
+}
