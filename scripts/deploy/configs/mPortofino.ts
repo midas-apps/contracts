11339b491e60f287e59d8b1d00c25846b9177558
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-10-28 18:46:41 +0200

    feat: mPortofino on ethereum

---
+++ scripts/deploy/configs/mPortofino.ts
@@ -0,0 +1,89 @@
+import { constants } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
+
+import { chainIds } from '../../../config';
+import { DeploymentConfig } from '../common/types';
+
+export const mPortofinoDeploymentConfig: DeploymentConfig = {
+  genericConfigs: {
+    customAggregator: {
+      maxAnswerDeviation: parseUnits('0.36', 8),
+      description: 'mPortofino/USD',
+    },
+    dataFeed: {},
+  },
+  networkConfigs: {
+    [chainIds.main]: {
+      dv: {
+        type: 'REGULAR',
+        enableSanctionsList: true,
+        feeReceiver: '0x8695F4014bF7D3f5DC9b154A22600782F6cdE4aD',
+        tokensReceiver: '0x4754940eeEc30e98A742b950A5ddf1835db74Dd7',
+        instantDailyLimit: constants.MaxUint256,
+        instantFee: parseUnits('0', 2),
+        variationTolerance: parseUnits('0.8', 2),
+        minMTokenAmountForFirstDeposit: parseUnits('0', 18),
+        maxSupplyCap: constants.MaxUint256,
+      },
+      rvSwapper: {
+        type: 'SWAPPER',
+        feeReceiver: '0x0461bD693caE49bE9d030E5c212e080F9c78B846',
+        tokensReceiver: '0x4754940eeEc30e98A742b950A5ddf1835db74Dd7',
+        requestRedeemer: '0xAe5049433428dB31D334Eb04749F6367B6893c0a',
+        instantDailyLimit: constants.MaxUint256,
+        instantFee: parseUnits('0.5', 2),
+        variationTolerance: parseUnits('0.8', 2),
+        liquidityProvider: '0x0461bD693caE49bE9d030E5c212e080F9c78B846',
+        enableSanctionsList: true,
+        swapperVault: {
+          mToken: 'mTBILL',
+          redemptionVaultType: 'redemptionVaultUstb',
+        },
+      },
+      postDeploy: {
+        addPaymentTokens: {
+          vaults: [
+            {
+              paymentTokens: [
+                {
+                  token: 'usdc',
+                  allowance: parseUnits('1000000000', 18),
+                },
+                {
+                  token: 'usdt',
+                  allowance: parseUnits('1000000000', 18),
+                },
+              ],
+              type: 'depositVault',
+            },
+            {
+              paymentTokens: [
+                {
+                  token: 'usdc',
+                  allowance: parseUnits('1000000000', 18),
+                },
+                {
+                  token: 'usdt',
+                  allowance: parseUnits('1000000000', 18),
+                },
+              ],
+              type: 'redemptionVaultSwapper',
+            },
+          ],
+        },
+        grantRoles: {
+          tokenManagerAddress: '0x8781bC62573e524306bBB05ef94c7A7E141ce00D',
+          vaultsManagerAddress: '0x2ACB4BdCbEf02f81BF713b696Ac26390d7f79A12',
+          oracleManagerAddress: '0x8F2e0d2aF1102033C363331f213c62BE2e6DF043',
+        },
+        setRoundData: {
+          data: parseUnits('1', 8),
+        },
+        pauseFunctions: {
+          depositVault: ['depositRequest', 'depositRequestWithCustomRecipient'],
+          redemptionVaultSwapper: ['redeemFiatRequest'],
+        },
+      },
+    },
+  },
+};
