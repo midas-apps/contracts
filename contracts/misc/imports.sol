bf78a1a3fc2bc7d357991e16edfb496a52541a2e
kostyamospan <kostyamospan@users.noreply.github.com>
2025-12-01 17:48:08 +0200

    fix: license

--- contracts/misc/imports.sol
+++ contracts/misc/imports.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
