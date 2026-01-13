28941f863deabbc85fb197575454b87de6ce5dd7
kostyamospan <kostyamospan@users.noreply.github.com>
2026-01-13 16:34:59 +0200

    chore: mkrapha feed upgrade

--- contracts/products/mKRalpha/MKRalphaCustomAggregatorFeed.sol
+++ contracts/products/mKRalpha/MKRalphaCustomAggregatorFeed.sol
@@ -22,6 +22,44 @@ contract MKRalphaCustomAggregatorFeed is
     /**
      * @inheritdoc CustomAggregatorV3CompatibleFeed
      */
+    function initialize(
+        address _accessControl,
+        int192 _minAnswer,
+        int192 _maxAnswer,
+        uint256 _maxAnswerDeviation,
+        string calldata _description
+    ) public override {
+        super.initialize(
+            _accessControl,
+            _minAnswer,
+            _maxAnswer,
+            _maxAnswerDeviation,
+            _description
+        );
+        // call v2 to increase contract version to 2
+        initializeV2(_maxAnswerDeviation);
+    }
+
+    /**
+     * @notice initializes the contract with a new max answer deviation
+     * @dev increases contract version to 2
+     * @param _newMaxAnswerDeviation new max answer deviation
+     */
+    function initializeV2(uint256 _newMaxAnswerDeviation)
+        public
+        reinitializer(2)
+    {
+        require(
+            _newMaxAnswerDeviation <= 100 * (10**decimals()),
+            "CA: !max deviation"
+        );
+
+        maxAnswerDeviation = _newMaxAnswerDeviation;
+    }
+
+    /**
+     * @inheritdoc CustomAggregatorV3CompatibleFeed
+     */
     function feedAdminRole() public pure override returns (bytes32) {
         return M_KRALPHA_CUSTOM_AGGREGATOR_FEED_ADMIN_ROLE;
     }
