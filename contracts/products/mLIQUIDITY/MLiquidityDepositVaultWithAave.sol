3cd22fdcfad17187253fe1e1d656391643eec5f0
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2026-04-15 14:45:31 +0300

    feat: mLIQUIDITY aave vaults

---
+++ contracts/products/mLIQUIDITY/MLiquidityDepositVaultWithAave.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "../../DepositVaultWithAave.sol";
+import "./MLiquidityMidasAccessControlRoles.sol";
+
+/**
+ * @title MLiquidityDepositVaultWithAave
+ * @notice Smart contract that handles mLIQUIDITY minting with Aave V3 auto-invest
+ * @author RedDuck Software
+ */
+contract MLiquidityDepositVaultWithAave is
+    DepositVaultWithAave,
+    MLiquidityMidasAccessControlRoles
+{
+    /**
+     * @dev leaving a storage gap for futures updates
+     */
+    uint256[50] private __gap;
+
+    /**
+     * @inheritdoc ManageableVault
+     */
+    function vaultRole() public pure override returns (bytes32) {
+        return M_LIQUIDITY_DEPOSIT_VAULT_ADMIN_ROLE;
+    }
+}
